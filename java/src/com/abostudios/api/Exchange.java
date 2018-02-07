aa7d119a4d21f49e479156bc2c02942b468307de
boz95 <a.a.bozhenok@ncl.ac.uk>
2018-02-07 22:44:25 +0000

    Initial project structure

---
+++ java/src/com/abostudios/api/Exchange.java
@@ -0,0 +1,7 @@
+package com.abostudios.api;
+
+/**
+ * Created by Artemiy on 07/02/2018.
+ */
+public interface Exchange {
+}
---
+++ java/test/com/abostudios/api/exchanges/TestBinance.java
@@ -0,0 +1,7 @@
+package com.abostudios.api.exchanges;
+
+/**
+ * Created by Artemiy on 07/02/2018.
+ */
+public class TestBinance {
+}
